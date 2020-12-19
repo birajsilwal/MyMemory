be9cab72cc11f2be70ce8d89a43c933477172bdc
Biraj Silwal <birajsilwal.ai@gmail.com>
2020-12-19 01:08:35 -0700

    created nice cards - need to make it dynamic

--- app/src/main/java/com/birajsilwal/mymemory/MainActivity.kt
+++ app/src/main/java/com/birajsilwal/mymemory/MainActivity.kt
@@ -9,21 +9,19 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
 
     // lateinit -> late initialization
-    private lateinit var rvBoard : RecyclerView
-    private lateinit var tvNumMoves : TextView
-    private lateinit var tvNumPairs : TextView
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         rvBoard = findViewById(R.id.rvBoard)
         tvNumMoves = findViewById(R.id.tvNumMoves)
         tvNumPairs = findViewById(R.id.tvNumPairs)
 
         rvBoard.adapter = MemoryBoardAdapter(this, 8)
         rvBoard.setHasFixedSize(true)
-        // this is the context -> Main activity context
         rvBoard.layoutManager = GridLayoutManager(this, 2)
     }
 }
--- app/src/main/java/com/birajsilwal/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/birajsilwal/mymemory/MemoryBoardAdapter.kt
@@ -1,16 +1,35 @@
 package com.birajsilwal.mymemory
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+    // it's like static variable in java
+    companion object {
+        private const val TAG = "MemoryBoardAdapter"
+        private const val MARGIN_SIZE = 10
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view : View = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val cardWidth = parent.width / 2 - 2 * MARGIN_SIZE
+        val cardHeight = parent.height / 4 - 2 * MARGIN_SIZE
+        val cardSideLength = min(cardWidth, cardHeight)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        // left top right down
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        // Set the appropriate width/height of the view
         return ViewHolder(view)
     }
 
@@ -22,9 +41,14 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
 
     // thie viewholder inherits rycyclerview viewholder
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(position: Int) {
-            TODO("Not yet implemented")
-        }
-    }
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
 
+        fun bind(position: Int) {
+            imageButton.setImageResource(R.drawable.ic_launcher_background)
+            imageButton.setOnClickListener {
+                Log.i(TAG, "Clicked on position $position")
+            }
+        }
+
+    }
 }
