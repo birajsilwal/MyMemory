0e3ba2c6143cef062598c72b53e6536fe6c608e8
Biraj Silwal <birajsilwal.ai@gmail.com>
2020-12-20 03:48:30 -0700

    created data class to represent one memory card

---
+++ app/src/main/java/com/birajsilwal/mymemory/models/MemoryCard.kt
@@ -0,0 +1,10 @@
+package com.birajsilwal.mymemory.models
+
+/*data class to represent one memory card*/
+data class MemoryCard(
+        // value of val can not be changes once set
+        val identifier : Int,
+        // value of var can be changed
+        var isFaceUp : Boolean = false,
+        var isMatched : Boolean = false
+)
