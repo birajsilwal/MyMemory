8e8662d191cae9b5e8e48988b3b0e884cca2ff11
Biraj Silwal <birajsilwal.ai@gmail.com>
2020-12-28 06:08:55 -0700

    enable save button and downscale images

--- app/src/main/java/com/birajsilwal/mymemory/CreateActivity.kt
+++ app/src/main/java/com/birajsilwal/mymemory/CreateActivity.kt
@@ -4,21 +4,29 @@ import android.app.Activity
 import android.content.ClipData
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
-import android.widget.GridLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.birajsilwal.mymemory.models.BoardSize
+import com.birajsilwal.mymemory.utils.BitmapScaler
 import com.birajsilwal.mymemory.utils.EXTRA_BOARD_SIZE
 import com.birajsilwal.mymemory.utils.isPermissionGranted
 import com.birajsilwal.mymemory.utils.requestPermission
+import java.io.ByteArrayOutputStream
 
 /*This is where user can create there own board*/
 class CreateActivity : AppCompatActivity() {
@@ -28,6 +36,8 @@ class CreateActivity : AppCompatActivity() {
         private const val PICK_PHOTO_CODE = 655
         private const val READ_EXTERNAL_PHOTO_CODE = 248
         private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+        private const val MIN_GAME_NAME_LENGTH = 3
+        private const val Max_GAME_NAME_LENGTH = 14
     }
 
     // late init -> declare them as member variable and
@@ -38,7 +48,7 @@ class CreateActivity : AppCompatActivity() {
 
     private lateinit var adapter: ImagePickerAdapter
     private lateinit var boardSize: BoardSize
-    private var numImagesRequired  = -1
+    private var numImagesRequired = -1
     private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,8 +64,25 @@ class CreateActivity : AppCompatActivity() {
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
 
+        // on click image should be saved in firebase database
+        btnSave.setOnClickListener {
+            saveDataToFireBase()
+        }
+
+        // create toast if text is > 14
+        etGameName.filters = arrayOf(InputFilter.LengthFilter(Max_GAME_NAME_LENGTH))
+        etGameName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                btnSave.isEnabled = shouldEnableSaveButton()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+
         // when user clicks the gray square on create own board
-        adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object: ImagePickerAdapter.ImageClickListener {
+        adapter = ImagePickerAdapter(this, chosenImageUris, boardSize, object : ImagePickerAdapter.ImageClickListener {
             override fun onPlaceHolderClicked() {
                 if (isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION)) {
                     launchIntentForPhotos()
@@ -123,8 +150,17 @@ class CreateActivity : AppCompatActivity() {
         btnSave.isEnabled = shouldEnableSaveButton()
     }
 
+    // check if you should enable or not
     private fun shouldEnableSaveButton(): Boolean {
-        // check if you shoule enable or not
+        // number of chose image should be equal to the required image
+        if (chosenImageUris.size != numImagesRequired) {
+            return false
+        }
+        // name of the game must not be blank and less than 3
+        if (etGameName.text.isBlank() || etGameName.text.length < MIN_GAME_NAME_LENGTH) {
+            return false
+        }
+        // otherwise enable the button
         return true
     }
 
@@ -133,5 +169,30 @@ class CreateActivity : AppCompatActivity() {
         intent.type = "image/*"
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         startActivityForResult(Intent.createChooser(intent, "Choose pics"), PICK_PHOTO_CODE)
+    }
+
+    private fun saveDataToFireBase() {
+        Log.i(TAG, "save data to firebase")
+        for ((index, photoUri) in chosenImageUris.withIndex()) {
+            // we are going to store image byte array into the database
+            val imageByteArray = getImageByteArray(photoUri)
+        }
+    }
+
+    // this function gets the original image and scales it down
+    private fun getImageByteArray(photoUri: Uri): ByteArray {
+        val originalBitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(contentResolver, photoUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
+        }
+        Log.i(TAG, "original width ${originalBitmap.width} and height ${originalBitmap.height}")
+        val scaledBitMap = BitmapScaler.scaleToFitHeight(originalBitmap, 250)
+        Log.i(TAG, "Scaled width ${scaledBitMap.width} and height ${scaledBitMap.height}")
+
+        val byteOutStream = ByteArrayOutputStream()
+        scaledBitMap.compress(Bitmap.CompressFormat.JPEG, 60, byteOutStream)
+        return byteOutStream.toByteArray()
     }
 }
